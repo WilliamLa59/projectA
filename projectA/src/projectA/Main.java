25ae5279dd53bb66f1ef7616658901d7e2a9f738
miguel mesa <mesa22@my.yorku.ca>
2021-04-17 05:27:36 -0400

    program now asks user for input and returns bmr based on input
    fixed errors on getBasalMetabolicRate, changed the conversion method
    into 2 separate conversion methods which are accessed by the
    getBasalMetabolicRate method, made everything static because it wont
    work otherwise.System works like this:  Main class takes variables,
    feeds them to formulas class which uses them as needed for
    getBasalMetabolicRate method, sends it back to main and prints it from
    there

--- projectA/src/projectA/Main.java
+++ projectA/src/projectA/Main.java
@@ -1,9 +1,52 @@
 package projectA;
-
+import java.util.Scanner;
+import java.io.File;
 public class Main {
-	
+
 	public static void main(String[] args) {
-		System.out.println("hi");
+		Scanner askInput = new Scanner(System.in);
+		
+		//gets gender input
+		System.out.println("Enter gender: ");
+		formulas.gender = askInput.nextLine();
+		System.out.println("gender = "+formulas.gender);
+		
+		//gets age input
+		System.out.println("Enter age: ");
+		formulas.age = askInput.nextInt();
+		System.out.println("age = "+formulas.age);
+		
+		//asks whether user wants to use imperial or metric
+		System.out.println("Will you give height/weight in Metric or Imperial? ");
+		System.out.println("Answer true for metric, false for imperial: ");
+		 formulas.metric= askInput.nextBoolean();
+		System.out.println("metric = "+formulas.metric);
+		
+		//gets height then weight in metric
+		if(formulas.metric == true) {
+			System.out.println("Enter height in cm: ");
+			formulas.heightMetric = askInput.nextDouble();
+			System.out.println("height = "+formulas.heightMetric);
+			
+			System.out.println("Enter weight in kg: ");
+			formulas.weightMetric = askInput.nextDouble();
+			System.out.println("weight = "+formulas.weightMetric);
+		}
+		//gets height then weight in imperial
+		else {
+			System.out.println("Enter height in inches");
+			formulas.heightImperialInches = askInput.nextDouble();
+			System.out.println("height = "+formulas.heightImperialInches);
+			
+			System.out.println("Enter weight in lbs: ");
+			formulas.weightImperial = askInput.nextDouble();
+			System.out.println("weight = "+formulas.weightImperial);
+		}
+		
+		System.out.println("thank you");
+		System.out.println("your Basal Metabolic Rate is: "+formulas.getBasalMetabolicRate());
+		System.out.println("uwu");
+		
 		 }
 	
 
--- projectA/src/projectA/formulas.java
+++ projectA/src/projectA/formulas.java
@@ -2,40 +2,63 @@ package projectA;
 
 public class formulas {
 	
-	double bmr;
-	double heightMetric;
-	double weightMetric;
-	double heightImperial;
-	double weightImperial;
-	double imperialToMetric;
+	static double bmr;
+	static double heightMetric;
+	static double weightMetric;
+	static double heightImperialFeet;
+	static double heightImperialInches;
+	static double weightImperial;
+	static double imperialToMetric;
+	static double convertedWeightMetric;
+	static double convertedHeightMetric;
+	static int age;
+	static boolean metric;
+	public static String gender;
 	
-	int age;
-	boolean metric;
-	String gender;
+	//converts imperial to metric
+	private static double getMetricHeight(){
+			 //adds the feet and inches given and returns total inches
+			 double heightImperialInchesTotal = heightImperialInches + heightImperialFeet*12;
+			 
+			 //converts total inches to total cm for height
+			 convertedHeightMetric = heightImperialInchesTotal*2.54;
+			 
+		return convertedHeightMetric;
+	}
+	
+	private static double getMetricWeight(){
+		
+			 //converts weight in lbs to kg
+			 convertedWeightMetric = weightImperial*0.45359237;
+		return convertedWeightMetric;	 
+	}
+	
+	
 	
 	//calculates basal metabolic rate
-	private double basalMetabolicRate() {
+	public static double getBasalMetabolicRate() {
+		
 		if(gender == "male") {
 			//male metric formula
-			if(metric = true) {
+			if(metric == true) {
 				bmr = 66.47+(13.75*weightMetric)+(5.003*heightMetric)-(6.755*age);
 			}
 			//male imperial formula
 			else {
-				bmr = 66.47+(13.75*weightImperial)+(5.003*heightImperial)-(6.755*age);
+				bmr = 66.47+(13.75 * getMetricWeight()) + (5.003 * getMetricHeight()) - (6.755 * age);
 			}
 		}
 		else {
 			//female metric formula
-			if(metric = true) {
+			if(metric == true) {
 				bmr = 655.1+(9.563*weightMetric)+(1.85*heightMetric)-(4.676*age);
 			}
 			//female imperial formula
 			else {
-				bmr = 655.1+(9.563*weightImperial)+(1.85*heightImperial)-(4.676*age);
+				bmr = 655.1+(9.563 * getMetricWeight())+(1.85 * getMetricHeight())-(4.676*age);
 			}
 		}
-		
-		return 4;
+		return bmr;
 	}
+	
 }
